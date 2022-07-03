733f51dd0b8d5b2cfbf54c23b3e7897bf3ea17e2
personal <sibinp90@gmail.com>
2022-07-03 14:18:37 +0530

    Day 4 of 30

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:catalogueapp/pages/home_page.dart';
+import 'package:catalogueapp/pages/login_page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_catalog/pages/home_page.dart';
-import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,16 +13,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: HomePage(), // no need of this since route / is there
-      themeMode: ThemeMode.dark,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      // home: HomePage(),
+      // no need of this since route / is there
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(
-          // brightness: Brightness.dark, // dark brightness will override any color given below
-          primarySwatch: Colors.blue),
-      initialRoute: "/home",
+        brightness: Brightness.dark,
+        // dark brightness will override any color given below
+        // primarySwatch: Colors.blue,
+      ),
+      initialRoute: "/",
       routes: {
-        "/": (context) =>
-            LoginPage(), // LoginPage is an object here and we denote it with () at the end
+        "/": (context) => LoginPage(),
+        // LoginPage is an object here and we denote it with () at the end
         "/home": (context) => HomePage(),
         "/login": (context) => LoginPage()
       },
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,14 +4,55 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-              fontSize: 20, color: Colors.blue, fontWeight: FontWeight.bold),
-          textScaleFactor: 1.0,
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter username",
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      print("Welcome Sibin");
+                    },
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
